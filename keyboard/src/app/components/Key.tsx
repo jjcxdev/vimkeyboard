338412005fc654a78056fee22ce59afb193344e9
jjcxdev <justinjchambers@gmail.com>
2024-02-25 22:42:45 -0500

    mode styles

--- keyboard/src/app/components/Key.tsx
+++ keyboard/src/app/components/Key.tsx
@@ -7,47 +7,37 @@ import {
 
 interface KeyProps {
   children?: ReactNode;
-  action?: string;
+  action: string;
   mode?: "normal" | "visual" | "insert";
   isPressed?: boolean;
 }
 
 export default function Key({ children, action, mode, isPressed }: KeyProps) {
-  let backgroundColor = "transparent";
+  // Initial styling, nothing additional applied
+  let additionalModeClass = "";
+
+  const isActiveInMode =
+    (mode === "normal" && NormalModeKeyLabels[action]?.normal) ||
+    (mode === "visual" && VisualModeKeyLabels[action]?.visual) ||
+    (mode === "insert" && InsertModeKeyLabels[action]?.insert);
+
+  if (isActiveInMode) {
+    additionalModeClass = ` key-${mode}`; // Apply only if key is active in current mode
+  }
+
+  // Construct className with base key class and additional mode class
+  const className = `key${additionalModeClass}`;
+
   // DO NOT REMOVE THIS - GLOBAL KEYPRESS REGARDLESS OF MODE
   let borderColor = isPressed ? "yellow" : ""; // yellow when pressed or inital styling
 
-  console.log(`Key: ${children}, Action: ${action}, Mode: ${mode}`);
-
-  if (action && mode) {
-    const isActiveInMode =
-      (mode === "normal" && NormalModeKeyLabels[action]?.normal) ||
-      (mode === "visual" && VisualModeKeyLabels[action]?.visual) ||
-      (mode === "insert" && InsertModeKeyLabels[action]?.insert);
-
-    if (isActiveInMode) {
-      switch (mode) {
-        case "normal":
-          backgroundColor = "red";
-          break;
-        case "visual":
-          backgroundColor = "blue";
-          break;
-        case "insert":
-          backgroundColor = "green";
-          break;
-        default:
-          backgroundColor = "transparent";
-      }
-    }
-  }
+  //
 
   // Pass GLOBAL KEY PRESS to keys
   return (
     <kbd
-      className="key"
+      className={className}
       style={{
-        backgroundColor,
         borderColor: borderColor,
         borderStyle: "solid",
         borderWidth: "1px",
